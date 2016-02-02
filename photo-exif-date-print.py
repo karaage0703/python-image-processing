5c8cc07492caa9b4f65a3412b3b575507b672a32
karaage0703 <coldchicken@gmail.com>
2016-02-02 23:42:18 +0900

    complete program

--- photo-exif-date-print.py
+++ photo-exif-date-print.py
@@ -1,26 +1,21 @@
 # -*- coding: utf-8 -*-
-# import cv2
-
+import cv2
 import sys
+import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 from PIL.ExifTags import TAGS, GPSTAGS
 
-font_size = 32
-font_opacity = 64
 font_color = (255, 255, 255)
 
 def get_exif(file,field):
-
     img = Image.open(file)
     exif = img._getexif()
-
 
     exif_data = []
     for id, value in exif.items():
         if TAGS.get(id) == field:
             tag = TAGS.get(id, id),value
             exif_data.extend(tag)
-
 
     return exif_data
 
@@ -44,29 +39,22 @@ def get_date_of_image(file):
     return exif_table.get("DateTimeOriginal")
 
 def put_date(file, date):
+    base_img_cv2 = cv2.imread(file)
+
     base_img = Image.open(file).convert('RGBA')
-    txt = Image.new('RGBA', base_img.size, (255, 255, 255, 0))
+    txt = Image.new('RGB', base_img.size, (0, 0, 0))
     draw = ImageDraw.Draw(txt)
-    fnt = ImageFont.truetype('./Arial Black.ttf', size=font_size)
+    fnt = ImageFont.truetype('./Arial Black.ttf', size=(int)((base_img.size[0]+base_img.size[1])/100))
 
     textw, texth = draw.textsize(date, font=fnt)
 
-    # draw.text(((base_img.width - textw) / 2, (base_img.height - texth) / 2),
-              # date, font=fnt, fill=color + (opacity,))
-    # draw.text(0, 0, date, font=fnt, fill=color)
-    # draw.text((0, 0), date, font=fnt, fill=(255,255,255))
-    draw.text((0, 0), date, font=fnt, fill=(0,0,0))
+    draw.text(((base_img.size[0]*0.95 - textw) , (base_img.size[1]*0.95 - texth)),
+              date, font=fnt, fill=font_color)
 
+    txt_array = np.array(txt)
 
-    # base_img.show()
-    # txt.show()
-    output_img = Image.alpha_composite(base_img, txt)
-    # base_img.alpha_composite(txt, (0,0))
-    output_img.show()
-    # return output_img
-
-        
-
+    output_img = cv2.addWeighted(base_img_cv2, 1.0, txt_array, 1.0, 0)
+    return output_img
 
 if __name__ == '__main__':
     param = sys.argv
@@ -76,3 +64,4 @@ if __name__ == '__main__':
 
     date = get_date_of_image(param[1])
     output_img = put_date(param[1], date)
+    cv2.imwrite('exifdata_' + param[1], output_img)
