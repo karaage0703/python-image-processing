101a8dd9a31a02943c0ca2b9d9480a72a7051e76
karaage0703 <coldchicken@gmail.com>
2017-07-18 01:26:25 +0900

    simplify script

--- face_crop_simple-all.sh
+++ face_crop_simple-all.sh
@@ -2,53 +2,12 @@
 
 cmd=face_crop_simple.py
 echo face crop processing...
-for f in *.JPG
+for f in *.jpg *.jpeg *.JPG *.JPEG *.png *.PNG
 do
 	if [ -f $f ]; then
 		echo python $cmd $f
 		python $cmd $f
 	fi
 done
-
-for f in *.jpg
-do
-	if [ -f $f ]; then
-		echo python $cmd $f
-		python $cmd $f
-	fi
-done
-
-for f in *.JPEG
-do
-	if [ -f $f ]; then
-		echo python $cmd $f
-		python $cmd $f
-	fi
-done
-
-for f in *.jpeg
-do
-	if [ -f $f ]; then
-		echo python $cmd $f
-		python $cmd $f
-	fi
-done
-
-for f in *.png
-do
-	if [ -f $f ]; then
-		echo python $cmd $f
-		python $cmd $f
-	fi
-done
-
-for f in *.PNG
-do
-	if [ -f $f ]; then
-		echo python $cmd $f
-		python $cmd $f
-	fi
-done
-
 
 echo done
