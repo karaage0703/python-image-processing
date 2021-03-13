c40dd88cf2b98523927f6add35ae736098049cbd
karaage0703 <coldchicken@gmail.com>
2021-03-14 01:13:32 +0900

    Refactoring

--- resize.py
+++ resize.py
@@ -2,27 +2,40 @@
 import cv2
 import sys
 
+
 def resize(src, w_ratio, h_ratio):
+    """Resize image
+
+    Args:
+        src (numpy.ndarray): Image
+        w_ratio (int): Width ratio
+        h_ratio (int): Height ratio
+
+    Returns:
+        numpy.ndarray: Image
+    """
+
     height = src.shape[0]
     width = src.shape[1]
-    dst = cv2.resize(src,((int)(width/100*w_ratio),(int)(height/100*h_ratio)))
+    dst = cv2.resize(src, ((int)(width/100*w_ratio), (int)(height/100*h_ratio)))
     return dst
+
 
 if __name__ == '__main__':
     param = sys.argv
-    if (len(param) != 4):
-        print ("Usage: $ python " + param[0] + " sample.jpg wide_ratio height_ratio")
+    if(len(param) != 4):
+        print("Usage: $ python " + param[0] + " sample.jpg wide_ratio height_ratio")
         quit()
 
     # open image file
     try:
         input_img = cv2.imread(param[1])
-    except:
-        print ('faild to load %s' % param[1])
+    except Exception as e:
+        print(e)
         quit()
 
     if input_img is None:
-        print ('faild to load %s' % param[1])
+        print('faild to load %s' % param[1])
         quit()
 
     w_ratio = int(param[2])
